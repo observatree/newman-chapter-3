d85f18eabef6218820b605761ee168ba63776812
Valentin Sponer <valentinsponer@Valentins-MacBook-Pro.local>
2018-03-15 11:27:37 -0700

    These changes may conflict with Brian's.

--- python-graphics/plotting_sunspot_data.py
+++ python-graphics/plotting_sunspot_data.py
@@ -1,20 +1,11 @@
 
+from pylab import plot, show
 from numpy import loadtxt
-
 import sys
 
-from pylab import plot, ylim, show
-# noinspection PyUnresolvedReferences
-from numpy import linspace, sin
-
 data = loadtxt(sys.argv[1], float)
-
-x = linspace(0, 10, 100)
-
-y = sin(x)
-
+print(data)
+x = data[:, 0]
+y = data[:, 1]
 plot(x, y)
-
-ylim(-1.1, 1.1)
-
 show()
