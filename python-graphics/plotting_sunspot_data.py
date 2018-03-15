1d54ba47fceefee637384522f4494b92c3ff2ea2
Brian Hill <brian@lingerhere.org>
2018-03-15 11:24:25 -0700

    Got show of a sin function to work.

--- python-graphics/plotting_sunspot_data.py
+++ python-graphics/plotting_sunspot_data.py
@@ -1,2 +1,20 @@
-from matplotlib import plotlib
 
+from numpy import loadtxt
+
+import sys
+
+from pylab import plot, ylim, show
+# noinspection PyUnresolvedReferences
+from numpy import linspace, sin
+
+data = loadtxt(sys.argv[1], float)
+
+x = linspace(0, 10, 100)
+
+y = sin(x)
+
+plot(x, y)
+
+ylim(-1.1, 1.1)
+
+show()
